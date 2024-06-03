1728da036821e932729eef2458347f82c6afbcc6
Yuval Reuveni <reuvyuv@post.bgu.ac.il>
2024-06-03 18:11:49 +0300

    ///

--- ex2.py
+++ ex2.py
@@ -12,43 +12,50 @@ diabetes_sklearn = load_diabetes()
 A = diabetes_sklearn.data
 b = diabetes_sklearn.target.reshape((-1,1))
 # Split the data into a training set and a testing set
-A_train, A_test, b_train, b_test = train_test_split(A, b, test_size=0.2, random_state=42)
+A_train, A_test, b_train, b_test = train_test_split(A, b, test_size=0.2)
 
 
 def gradient_descent(A_train, b_train, A_test, b_test):
     n, m = A_train.shape
-    epsilon = 0.01  # step size
-    delta = 1e-5    # stop conditions
+    epsilon = 0.01 # step size
+    delta = 5    # stop conditions
     #initialize the parameters
-    xk = np.zeros(m).reshape((-1,1))
+    x0 = np.zeros(m).reshape((-1,1))
+    xk = x0
     #objective function
-    err_fun = lambda x: np.linalg.norm(A_train.dot(x) - b_train)**2
+    train_err_fun = lambda x: np.linalg.norm(A_train.dot(x) - b_train)**2
+    test_err_fun = lambda x: np.linalg.norm(A_test.dot(x) - b_test)**2
     TRAIN_ERR = []
-    MSE = []
+    TEST_ERR = []
+    curr_train_err = train_err_fun(xk)
+    curr_test_err = test_err_fun(xk)
+    TRAIN_ERR.append(curr_train_err)
+    TEST_ERR.append(curr_test_err)
     #gradient descent
     grad = 2*A_train.T.dot(A_train.dot(xk) - b_train)
+
     while np.linalg.norm(grad) > delta:
-        err = err_fun(xk)
-        TRAIN_ERR.append(err)
-        xk = xk - epsilon * grad
         grad = 2*A_train.T.dot(A_train.dot(xk) - b_train)
+        xk = xk - epsilon * grad
         # Evaluate on the test set
         # Prediction
-        b_pred = A_test.dot(xk)
-        mse = np.mean((b_test - b_pred)**2)
-        MSE.append(mse)
-        
+        train_err = train_err_fun(xk)
+        test_err = test_err_fun(xk)
+        TRAIN_ERR.append(train_err)
+        TEST_ERR.append(test_err)
+
         
     #plot the results with title and bar title
     #the error plot of this procedure, namely a graph of the error l2 |A@xk − b|**2 where xk is the point at step k.
     # show a graph of the train error jointly with the graph of the test error
-    plt.plot(MSE), plt.plot(TRAIN_ERR)
-    ERR = np.stack(TRAIN_ERR)
+    plt.figure(figsize=(10, 6))
+    plt.plot(TEST_ERR), plt.plot(TRAIN_ERR)
     plt.legend(['Test Error', 'Train Error'])
     
     plt.title('Test Error vs Train Error')
     plt.xlabel('Iteration')
     plt.ylabel('Error') 
+    plt.grid(True)
     plt.show()
 
 gradient_descent(A_train, b_train, A_test, b_test)
