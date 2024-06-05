46068125b4e4600ed06f5d2213b8ea700d8b24fb
Yuval Reuveni <reuvyuv@post.bgu.ac.il>
2024-06-05 18:56:31 +0300

    //

--- ex2.py
+++ ex2.py
@@ -12,28 +12,27 @@ diabetes_sklearn = load_diabetes()
 A = diabetes_sklearn.data
 b = diabetes_sklearn.target.reshape((-1,1))
 # Split the data into a training set and a testing set
-A_train, A_test, b_train, b_test = train_test_split(A, b, test_size=0.2)
 
 
-def gradient_descent(A_train, b_train, A_test, b_test):
+def gradient_descent():
+    A_train, A_test, b_train, b_test = train_test_split(A, b, test_size=0.2)
     n, m = A_train.shape
     epsilon = 1e-3 # step size
     delta = 10    # stop conditions
     #initialize the parameters
     x0 = np.zeros(m).reshape((-1,1))
-    xk = x0
     #objective function
     train_err_fun = lambda x: np.linalg.norm(A_train.dot(x) - b_train)**2
     test_err_fun = lambda x: np.linalg.norm(A_test.dot(x) - b_test)**2
     TRAIN_ERR = []
     TEST_ERR = []
-    curr_train_err = train_err_fun(xk)
-    curr_test_err = test_err_fun(xk)
+    curr_train_err = train_err_fun(x0)
+    curr_test_err = test_err_fun(x0)
     TRAIN_ERR.append(curr_train_err)
     TEST_ERR.append(curr_test_err)
     #gradient descent
-    grad = 2*A_train.T.dot(A_train.dot(xk) - b_train)
-
+    grad = 2*A_train.T.dot(A_train.dot(x0) - b_train)
+    xk = x0
     while np.linalg.norm(grad) > delta:
         grad = 2*A_train.T.dot(A_train.dot(xk) - b_train)
         xk = xk - epsilon * grad
@@ -44,7 +43,6 @@ def gradient_descent(A_train, b_train, A_test, b_test):
         TRAIN_ERR.append(train_err)
         TEST_ERR.append(test_err)
 
-        
     #plot the results with title and bar title
     #the error plot of this procedure, namely a graph of the error l2 |A@xk − b|**2 where xk is the point at step k.
     # show a graph of the train error jointly with the graph of the test error
@@ -52,12 +50,12 @@ def gradient_descent(A_train, b_train, A_test, b_test):
     plt.plot(TEST_ERR), plt.plot(TRAIN_ERR)
     plt.legend(['Test Error', 'Train Error'])
     
-    plt.title('Test Error vs Train Error')
+    plt.title('Test Error vs Train Error over Iterations for Gradient Descent')
     plt.xlabel('Iteration')
     plt.ylabel('Error') 
     plt.grid(True)
     plt.show()
 
-gradient_descent(A_train, b_train, A_test, b_test)
+gradient_descent()
 
 
